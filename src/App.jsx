22b927c471ee22df6efc9eff054b5c30320181fe
Renixon Christian <renixon.silva@alunos.ufersa.edu.br>
2026-07-01 10:38:40 -0300

    Create App component with ConviteHemyly import

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ConviteHemyly from './convite-hemyly.jsx'
+
+function App() {
+  return (
+    <div>
+      <ConviteHemyly />
+    </div>
+  )
+}
+
+export default App
