96ecb1d481db52ccc73576039d498d17afd7ceba
Renixon Christian <renixon.silva@alunos.ufersa.edu.br>
2026-07-01 11:36:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // ATENÇÃO: Substitua abaixo pelo nome exato do seu repositório
-  base: '/Convite/' 
+  base: '/' 
 })
